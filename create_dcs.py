60f40e4936e028f8af267b2aacc264965c838b82
Pamela Fox <pamela.fox@gmail.com>
2023-06-19 20:38:15 +0000

    Add a devcontainer for every notebook

--- create_dcs.py
+++ create_dcs.py
@@ -1,35 +1,27 @@
 import os
 import json
 
-# recursively traverse materials directory
-# and find all ipynb files
-# create a dictionary of all ipynb files
-
+# Find all notebooks in materials/fa22
 notebooks = {}
 for root, dirs, files in os.walk('materials/fa22'):
     for file in files:
         if file.endswith('.ipynb'):
             path = os.path.join(root, file)
             notebooks[file] = path
-print(notebooks)
 
-# open .devcontainer/devcontainer.json
-# read json into a dictionary
+# Create a new devcontainer.json for each file in notebooks
 with open('.devcontainer/devcontainer.json', 'r') as f:
     dc = json.load(f)
     
-    # create a new devcontainer.json for each file in notebooks
     for file, path in notebooks.items():
-        # create a new devcontainer.json
+        assignment = file.split('.')[0]
+        # Customize the devcontainer.json dict with filename-based customizations
         new_dc = dc.copy()
-        new_dc['name'] = file
-        new_dc['customizations']['codespaces'] = {
-            'openFiles': [path]
-        }
-        # make directory for new devcontainer.json
-        # based on file name without extension
-        dir = os.path.join('.devcontainer', file.split('.')[0])
-        os.mkdir(dir)
-        # write new devcontainer.json to directory
+        new_dc['name'] = f"Data 8: {assignment}"
+        new_dc['postAttachCommand'] = f"code /workspaces/materials-fa22/{path}"
+        # Write new devcontainer.json into filename-based directory
+        dir = os.path.join('.devcontainer', assignment)
+        if not os.path.exists(dir):
+            os.mkdir(dir)
         with open(os.path.join(dir, 'devcontainer.json'), 'w') as f:
             json.dump(new_dc, f, indent=4)
