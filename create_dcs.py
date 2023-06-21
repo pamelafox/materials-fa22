591f5fad4b50dd0a1619e6ce09627176562ceb5b
Pamela Fox <pamela.fox@gmail.com>
2023-06-21 20:46:58 +0000

    Dev container updates

--- create_dcs.py
+++ create_dcs.py
@@ -10,6 +10,7 @@ for root, dirs, files in os.walk('materials/fa22'):
             notebooks[file] = path
 
 # Create a new devcontainer.json for each file in notebooks
+links = {} # {assignment: {notebook: path, devcontainer: path}}
 with open('.devcontainer/devcontainer.json', 'r') as f:
     dc = json.load(f)
     
@@ -27,5 +28,20 @@ with open('.devcontainer/devcontainer.json', 'r') as f:
         dir = os.path.join('.devcontainer', assignment)
         if not os.path.exists(dir):
             os.mkdir(dir)
-        with open(os.path.join(dir, 'devcontainer.json'), 'w') as f:
+        new_dc_path = os.path.join(dir, 'devcontainer.json')
+        with open(new_dc_path, 'w') as f:
             json.dump(new_dc, f, indent=4)
+        links[assignment] = {
+            'notebook': path,
+            'devcontainer': new_dc_path
+        }
+
+# Create a table of links in links.md
+with open('links.md', 'w') as f:
+    f.write("# Assignments \n\n")
+    f.write("| Assignment | Link |\n")
+    f.write("| --- | --- |\n")
+    base_path = f"https://codespaces.new/pamelafox/materials-fa22?quickstart=1&devcontainer_path="
+
+    for assignment, paths in links.items():
+        f.write(f"| [{assignment}]({paths['notebook']}]) | [![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)]({base_path}{paths['devcontainer']}) |\n")
